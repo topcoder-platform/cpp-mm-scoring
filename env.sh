de746a2066363511a51dbbfbdee050f18f0cdf34
cccvvv2012 <cccvvv2012@163.com>
2018-11-16 03:25:10 -0800

    cleanup

--- env.sh
+++ env.sh
@@ -14,4 +14,3 @@ export LLVM_INSTALL_PREFIX="$HOME/.cpp-mm-scoring/cling-0.5"
 export CLING_LIB_DIR="$HOME/.cpp-mm-scoring/cling-0.5/lib"
 export NLOHMANN_JSON_INCLUDE_DIR="$HOME/.cpp-mm-scoring/include"
 export BOOST_DIR="$HOME/.cpp-mm-scoring/boost"
-#export SKIP_DOWNLOAD=y
